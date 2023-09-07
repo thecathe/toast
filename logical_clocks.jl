6c439d71e1ae73f86923f30211f8f27bd8b3cf4a
Jonah Pears <pearsandcabbage@gmail.com>
2023-09-07 19:31:57 +0100

    cleaning up

--- logical_clocks.jl
+++ logical_clocks.jl
@@ -32,41 +32,6 @@ module LogicalClocks
     #
     include("logical_clocks/clock_valuations.jl")
     using .ClockValuations
-    export Valuations
-
-
-    export value!, reset!, time_step!, labels
-
-    
-    # Base.convert(::Type{UnitOfTime}, t::T) where {T<:Int} = UnitOfTime(t)
-
-    # returns value of clock, instansiating to offset if not existing
-    value!(c::Clocks,l::String,offset::T) where {T<:Number} = value!(c,Label(l),ClockValue(offset))
-    function value!(c::Clocks, l::Label, offset::ClockValue = ClockValue(0))
-        if l in labels(c)
-            for x in c.children
-                if x.label == l
-                    return (ClockValue(x.value), l, true)
-                end
-            end
-        else
-            push!(c, Clock(l, offset))
-            return (ClockValue(offset), l, false)
-        end
-    end
-
-    # show value correctly
-    Base.show(val::Tuple{ClockValue,Label,Bool},io::Core.IO = stdout) = print(io, string(val))
-    Base.show(val::Tuple{ClockValue,Label,Bool}, verbose::Bool ,io::Core.IO = stdout) = print(io, string(val,verbose))
-    Base.string(val::Tuple{ClockValue,Label,Bool}, verbose::Bool = true) = string(string(Clock(val[2],val[1])), val[3] && verbose ? "" : " *fresh*")
-
-    # resets clocks with labels to 0
-    reset!(c::Clocks, l::T) where {T<:Array{Any}} = reset!(c,Labels(l))
-    reset!(c::Clocks, l::T) where {T<:Array{String}} = reset!(c,Labels(l))
-    reset!(c::Clocks, l::Labels) = foreach(a -> if (value!(c,a) == (~,~,true))  getindex(c,findfirst(x -> x.label == l, c)).value = ClockValue(0) end, l)
-
-    # passes time over clocks
-    time_step!(c::Clocks, t::T) where {T<:Integer} = time_step!(c,UnitOfTime(t))
-    time_step!(c::Clocks, t::UnitOfTime) = foreach(x -> x.value += t.value, c)
+    export Valuations, ValueOf!, ResetClocks!, TimeStep!
 
 end
